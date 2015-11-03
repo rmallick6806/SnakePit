4ebf1d72cebc272b906082a31f6780bf8372fcee
Mark F. Ellis II <mellis@pros.com>
2015-11-03 11:55:26 -0600

    update webpack config

--- webpack.config.devSockets.js
+++ webpack.config.devSockets.js
@@ -1,1 +1,24 @@
-webpack.config.devSockets.js
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        './src/js/app'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'snake.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.less$/,
+            loader: 'style!css!less',
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
