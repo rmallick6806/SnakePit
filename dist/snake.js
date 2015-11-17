4139a438fda9ecb74a6b40db40486a08303c7485
Ray Malllick <rmallick@triadinteractive.com>
2015-11-16 23:09:01 -0500

    Cleaning up code
    Adding second snake with varied speed abilities. draw method just
    works. Clear method is also working.

--- dist/snake.js
+++ dist/snake.js
@@ -76,7 +76,7 @@
 	canvas.width = 780;
 	
 	const SnakePit = exports.SnakePit = {};
-	SnakePit.fps = 8;
+	SnakePit.fps2 = 3;
 	SnakePit.cellWidth = 13;
 	
 	SnakePit.game = function () {
@@ -85,20 +85,31 @@
 		let gameRunning = true;
 		let food = new SnakePit.food();
 		// init snake object
-		let snake1 = new SnakePit.snake();
+		let snake1 = new SnakePit.snake({
+			x: 20,
+			y: 20,
+			speed: 6
+		});
+		let snake2 = new SnakePit.snake({
+			x: 20,
+			y: 30,
+			speed: 6
+		});
 		var gameTime = Date.now();
 	
 		function init() {
 			bindEvents();
 			snake1.init();
+			snake2.init();
 			food.place();
 			gameLoop();
+			gameLoop2();
 		}
 	
 		function update(now, snake) {
 			advanceSnake(now, snake);
 			checkCollision(snake);
-			//checkSelfCollision(snake);
+			checkSelfCollision(snake);
 		}
 	
 		function advanceSnake(now, snake) {
@@ -124,7 +135,6 @@
 			} else {
 				snake.segments.pop();
 			}
-			console.log(now);
 		}
 	
 		function checkCollision(snake) {
@@ -148,7 +158,7 @@
 			let head = snake.segments._head.data;
 			if (_lodash2.default.isEqual(head, food.coordinates)) {
 				food.place();
-				if (SnakePit.fps < 60) SnakePit.fps += 1;
+				if (SnakePit.fps < 60) snake.speed += 1;
 				return true;
 			}
 		}
@@ -160,6 +170,14 @@
 			snake1.segments.forEach(function (segment, index) {
 				ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake1.segmentSize, snake1.segmentSize);
 				ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake1.segmentSize, snake1.segmentSize);
+			});
+	
+			ctx.fillStyle = 'blue';
+			ctx.strokeStyle = "white";
+	
+			snake2.segments.forEach(function (segment, index) {
+				ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
+				ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
 			});
 	
 			ctx.fillStyle = 'red';
@@ -183,7 +201,21 @@
 			draw();
 			setTimeout(() => {
 				requestAnimationFrame(gameLoop);
-			}, 1000 / SnakePit.fps);
+			}, 1000 / snake1.speed);
+		}
+	
+		function gameLoop2() {
+			if (!gameRunning) return;
+	
+			var now = Math.round(Date.now() / 50);
+			var delta = (now - then) / 1000;
+			var then = now;
+			update(now, snake2);
+			clear();
+			draw();
+			setTimeout(() => {
+				requestAnimationFrame(gameLoop2);
+			}, 1000 / snake2.speed);
 		}
 	
 		function bindEvents() {
@@ -200,136 +232,6 @@
 	
 				if (direction) {
 					snake1.setDirection(direction);
-				} else if (key === 32) {
-					gameRunning = false;
-				}
-			});
-		}
-		return {
-			init: init
-		};
-	};
-	
-	SnakePit.game2 = function () {
-		// Create the canvas
-		// game configuration
-		let gameRunning = true;
-		let food = new SnakePit.food();
-		// init snake object
-		let snake2 = new SnakePit.snake2();
-		var gameTime = Date.now();
-	
-		function init() {
-			bindEvents();
-			snake2.init();
-			food.place();
-			gameLoop();
-		}
-	
-		function update(now, snake) {
-			advanceSnake(now, snake);
-			checkCollision(snake);
-			//checkSelfCollision(snake);
-		}
-	
-		function advanceSnake(now, snake) {
-			let newSnakeHeadPosition = {
-				x: snake.segments._head.data.x,
-				y: snake.segments._head.data.y
-			};
-			let vectors = {
-				RIGHT: { x: 1, y: 0 },
-				LEFT: { x: -1, y: 0 },
-				UP: { x: 0, y: -1 },
-				DOWN: { x: 0, y: 1 }
-			};
-			let currentVector = vectors[snake.direction];
-	
-			if (currentVector) {
-				newSnakeHeadPosition.x += currentVector.x;
-				newSnakeHeadPosition.y += currentVector.y;
-			}
-	
-			snake.segments.unshift(newSnakeHeadPosition);
-			if (checkFoodCollision(snake, food)) {
-				snake.length += 1;
-			} else {
-				snake.segments.pop();
-			}
-			console.log(now);
-		}
-	
-		function checkCollision(snake) {
-			let head = snake.segments._head.data;
-			if (head.x < 0 || head.y < 0 || head.x >= canvas.width / SnakePit.cellWidth || head.y >= canvas.height / SnakePit.cellWidth) {
-				gameRunning = false;
-			}
-		}
-	
-		function checkSelfCollision(snake) {
-			let head = snake.segments._head.data;
-			let noCollision = snake.segments.reduce((previousValue, currentSegment, index, segments) => {
-				let segmentsCollide = _lodash2.default.isEqual(head, currentSegment);
-				if (typeof previousValue === 'object') previousValue = true;
-				return previousValue && !segmentsCollide;
-			});
-			if (!noCollision) gameRunning = false;
-		}
-	
-		function checkFoodCollision(snake, food) {
-			let head = snake.segments._head.data;
-			if (_lodash2.default.isEqual(head, food.coordinates)) {
-				food.place();
-				if (SnakePit.fps < 60) SnakePit.fps += 1;
-				return true;
-			}
-		}
-	
-		function draw() {
-			ctx.fillStyle = 'green';
-			ctx.strokeStyle = "white";
-	
-			snake2.segments.forEach(function (segment, index) {
-				ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-				ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-			});
-	
-			ctx.fillStyle = 'red';
-			ctx.fillRect(food.coordinates.x * SnakePit.cellWidth, food.coordinates.y * SnakePit.cellWidth, 10, 10);
-			ctx.strokeRect(food.coordinates.x * SnakePit.cellWidth, food.coordinates.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-		}
-	
-		function clear() {
-			ctx.fillStyle = 'black';
-			ctx.fillRect(0, 0, canvas.height, canvas.width);
-		}
-	
-		function gameLoop() {
-			if (!gameRunning) return;
-	
-			var now = Math.round(Date.now() / 50);
-			var delta = (now - then) / 1000;
-			var then = now;
-			update(now, snake2);
-			draw();
-			setTimeout(() => {
-				requestAnimationFrame(gameLoop);
-			}, 1000 / 2);
-		}
-	
-		function bindEvents() {
-			let controls = {
-				37: 'LEFT',
-				38: 'UP',
-				39: 'RIGHT',
-				40: 'DOWN'
-			};
-	
-			document.addEventListener('keydown', function (event) {
-				let key = event.keyCode;
-				let direction = controls[key];
-	
-				if (direction) {
 					snake2.setDirection(direction);
 				} else if (key === 32) {
 					gameRunning = false;
@@ -342,48 +244,14 @@
 	};
 	
 	// Game objects
-	SnakePit.snake = function () {
+	SnakePit.snake = function (options) {
 		let snake = this;
 		this.head = {
-			x: 20,
-			y: 20
+			x: options.x,
+			y: options.y
 		};
 		this.segmentSize = 10;
-		this.speed = 1;
-		this.length = 5;
-		this.segments = new _fastList2.default();
-		this.direction = 'RIGHT';
-		this.init = function () {
-			_lodash2.default.range(snake.length).map(function (segment, index) {
-				snake.segments.push({
-					x: snake.head.x - index,
-					y: snake.head.y
-				});
-			});
-		};
-	
-		this.setDirection = function (newDirection) {
-			let oppositeDirections = {
-				LEFT: 'RIGHT',
-				RIGHT: 'LEFT',
-				UP: 'DOWN',
-				DOWN: 'UP'
-			};
-	
-			if (newDirection !== oppositeDirections[snake.direction]) {
-				snake.direction = newDirection;
-			}
-		};
-	};
-	
-	SnakePit.snake2 = function () {
-		let snake = this;
-		this.head = {
-			x: 20,
-			y: 30
-		};
-		this.segmentSize = 10;
-		this.speed = 1;
+		this.speed = options.speed;
 		this.length = 5;
 		this.segments = new _fastList2.default();
 		this.direction = 'RIGHT';
@@ -423,7 +291,6 @@
 	};
 	
 	SnakePit.game().init();
-	SnakePit.game2().init();
 
 /***/ },
 /* 2 */
--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ canvas.height = 780;
 canvas.width = 780;
 
 export const SnakePit = {};
-SnakePit.fps = 8;
+SnakePit.fps2 = 3;
 SnakePit.cellWidth = 13;
 
 SnakePit.game = function() {
@@ -17,20 +17,31 @@ SnakePit.game = function() {
 	let gameRunning = true;
 	let food = new SnakePit.food();
 	// init snake object
-	let snake1 = new SnakePit.snake();
+		let snake1 = new SnakePit.snake({
+		x: 20,
+		y: 20,
+		speed: 6
+	});
+		let snake2 = new SnakePit.snake({
+		x: 20,
+		y: 30,
+		speed: 6
+	});
 	var gameTime = Date.now();
 
 	function init() {
 		bindEvents();
 		snake1.init();
+		snake2.init();
 		food.place();
 		gameLoop();
+		gameLoop2();
 	}
 
 	function update(now, snake) {
 		advanceSnake(now, snake);
 		checkCollision(snake);
-		//checkSelfCollision(snake);
+		checkSelfCollision(snake);
 	}
 
 	function advanceSnake(now, snake) {
@@ -57,7 +68,6 @@ SnakePit.game = function() {
 		} else {
 			snake.segments.pop();
 		}
-	console.log(now);
 	}
 
 	function checkCollision(snake){
@@ -84,7 +94,7 @@ SnakePit.game = function() {
 		let head = snake.segments._head.data;
 		if ( _.isEqual(head, food.coordinates) ) {
 			food.place();
-			if (SnakePit.fps < 60) SnakePit.fps += 1;
+			if (SnakePit.fps < 60) snake.speed += 1;
 			return true;
 		}
 	}
@@ -97,6 +107,15 @@ SnakePit.game = function() {
 			ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake1.segmentSize, snake1.segmentSize);
 			ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake1.segmentSize, snake1.segmentSize);
 		});
+
+		ctx.fillStyle = 'blue';
+		ctx.strokeStyle = "white";
+
+		snake2.segments.forEach(function(segment, index){
+			ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
+			ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
+		});
+
 
 		ctx.fillStyle = 'red';
 		ctx.fillRect(food.coordinates.x * SnakePit.cellWidth, food.coordinates.y * SnakePit.cellWidth, 10, 10);
@@ -119,7 +138,21 @@ SnakePit.game = function() {
 	   	draw();
 	   	setTimeout( () => {
 	   		requestAnimationFrame(gameLoop);
-	   	}, 1000 / SnakePit.fps);
+	   	}, 1000 / snake1.speed);
+	}
+
+	function gameLoop2() {
+		if (!gameRunning) return;
+
+		var now = Math.round(Date.now()/50);
+		var delta = (now - then)/1000;
+		var then = now;
+		update(now, snake2);
+	   	clear();
+	   	draw();
+	   	setTimeout( () => {
+	   		requestAnimationFrame(gameLoop2);
+	   	}, 1000 / snake2.speed);
 	}
 
 	function bindEvents() {
@@ -136,141 +169,6 @@ SnakePit.game = function() {
 
 	      if (direction) {
 	        snake1.setDirection(direction);
-	      }
-	      else if (key === 32) {
-	        gameRunning = false;
-	      }
-	    });
-  	}
-  	return {
-  		init: init
-  	}
-};
-
-SnakePit.game2 = function() {
-	// Create the canvas
-	// game configuration
-	let gameRunning = true;
-	let food = new SnakePit.food();
-	// init snake object
-	let snake2 = new SnakePit.snake2();
-	var gameTime = Date.now();
-
-	function init() {
-		bindEvents();
-		snake2.init();
-		food.place();
-		gameLoop();
-	}
-
-	function update(now, snake) {
-		advanceSnake(now, snake);
-		checkCollision(snake);
-		//checkSelfCollision(snake);
-	}
-
-	function advanceSnake(now, snake) {
-		let newSnakeHeadPosition = {
-			x: snake.segments._head.data.x,
-			y: snake.segments._head.data.y
-		};
-		let vectors = {
-			RIGHT : { x: 1, y: 0 },
-			LEFT  : { x: -1, y: 0 },
-			UP    : { x: 0, y: -1 },
-			DOWN  : { x: 0, y: 1 }
-		};
-		let currentVector = vectors[snake.direction];
-	
-		if ( currentVector ) {
-			newSnakeHeadPosition.x += (currentVector.x);
-			newSnakeHeadPosition.y += (currentVector.y);
-		}
-	 
-	
-		snake.segments.unshift(newSnakeHeadPosition);
-		if( checkFoodCollision(snake, food)) {
-			snake.length += 1;
-		} else {
-			snake.segments.pop();
-		}
-	console.log(now);
-	}
-
-	function checkCollision(snake){
-		let head = snake.segments._head.data;
-		if ( head.x < 0 ||
-			 head.y < 0 ||
-			 head.x >= (canvas.width / SnakePit.cellWidth) ||
-			 head.y >= (canvas.height / SnakePit.cellWidth) ) {
-				gameRunning = false;
-		}
-	}
-
-	function checkSelfCollision(snake){
-		let head = snake.segments._head.data;
-		let noCollision = snake.segments.reduce( (previousValue, currentSegment, index, segments) => {
-			let segmentsCollide = _.isEqual(head, currentSegment);
-			if (typeof previousValue === 'object') previousValue = true;
-			return previousValue && !segmentsCollide;
-		});
-		if (!noCollision) gameRunning = false;
-	}
-
-	function checkFoodCollision(snake, food) {
-		let head = snake.segments._head.data;
-		if ( _.isEqual(head, food.coordinates) ) {
-			food.place();
-			if (SnakePit.fps < 60) SnakePit.fps += 1;
-			return true;
-		}
-	}
-
-	function draw() {
-		ctx.fillStyle = 'green';
-		ctx.strokeStyle = "white";
-
-		snake2.segments.forEach(function(segment, index){
-			ctx.fillRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-			ctx.strokeRect(segment.x * SnakePit.cellWidth, segment.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-		});
-
-		ctx.fillStyle = 'red';
-		ctx.fillRect(food.coordinates.x * SnakePit.cellWidth, food.coordinates.y * SnakePit.cellWidth, 10, 10);
-		ctx.strokeRect(food.coordinates.x * SnakePit.cellWidth, food.coordinates.y * SnakePit.cellWidth, snake2.segmentSize, snake2.segmentSize);
-	}
-
-	function clear() {
-		ctx.fillStyle = 'black';
-		ctx.fillRect(0,0, canvas.height, canvas.width);
-	}
-
-	function gameLoop() {
-		if (!gameRunning) return;
-
-		var now = Math.round(Date.now()/50);
-		var delta = (now - then)/1000;
-		var then = now;
-		update(now, snake2);
-	   	draw();
-	   	setTimeout( () => {
-	   		requestAnimationFrame(gameLoop);
-	   	}, 1000 / 2);
-	}
-
-	function bindEvents() {
-		let controls = {
-			37: 'LEFT',
-			38: 'UP',
-			39: 'RIGHT',
-			40: 'DOWN'
-		}
-
-	    document.addEventListener('keydown', function (event) {
-	      let key = event.keyCode;
-	      let direction = controls[key];
-
-	      if (direction) {
 	        snake2.setDirection(direction);
 	      }
 	      else if (key === 32) {
@@ -283,51 +181,15 @@ SnakePit.game2 = function() {
   	}
 };
 
-
 // Game objects
-SnakePit.snake = function() {
+SnakePit.snake = function(options) {
 	let snake = this;
 	this.head = {
-		x: 20,
-		y: 20
+		x: options.x,
+		y: options.y
 	};
 	this.segmentSize = 10;
-	this.speed = 1;
-	this.length = 5;
-	this.segments = new FastList;
-	this.direction = 'RIGHT';
-	this.init = function() {
-		_.range(snake.length)
-			.map(function(segment, index){
-				snake.segments.push({
-					x: snake.head.x - (index),
-					y: snake.head.y
-				});
-			});
-	}
-
-	this.setDirection = function(newDirection) {
-  		let oppositeDirections = {
-		  	LEFT: 'RIGHT',
-		  	RIGHT: 'LEFT',
-		  	UP: 'DOWN',
-		  	DOWN: 'UP'
-	  	}
-
-  		if (newDirection !== oppositeDirections[snake.direction]) {
-	  		snake.direction = newDirection;
-	  	}
-	}
-};
-
-SnakePit.snake2 = function() {
-	let snake = this;
-	this.head = {
-		x: 20,
-		y: 30
-	};
-	this.segmentSize = 10;
-	this.speed = 1;
+	this.speed = options.speed;
 	this.length = 5;
 	this.segments = new FastList;
 	this.direction = 'RIGHT';
@@ -368,4 +230,3 @@ SnakePit.food = function() {
 };
 
 SnakePit.game().init();
-SnakePit.game2().init();
