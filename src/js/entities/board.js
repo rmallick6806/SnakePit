f5a3d484547c73d1e25bc13db81ebcddb1abc511
Mark F. Ellis II <ellismarkf@gmail.com>
2015-11-19 23:15:36 -0600

    draw each snake in forEach loop

--- src/js/entities/board.js
+++ src/js/entities/board.js
@@ -1,31 +1,37 @@
+import _ from 'lodash';
+
 module.exports = function Board() {
 	let board = this;
 	this.cellWidth = 13;
 
-	this.draw = (ctx, snake1, snake2, food) => {
-		ctx.fillStyle = 'green';
+	this.draw = (ctx, snakes, food, elapsed) => {
 		ctx.strokeStyle = "white";
 
-		snake1.segments.forEach(function(segment, index){
-			ctx.fillRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake1.segmentSize, snake1.segmentSize);
-			ctx.strokeRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake1.segmentSize, snake1.segmentSize);
-		});
+		_.forEach(snakes, (snake, playerIndex) => {
+			let snakeColor = playerIndex === 0 ? 'green' : 'blue';
+			ctx.fillStyle = snakeColor;
+			snake.segments.forEach( (segment, segmentIndex) => {
+				ctx.fillRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake.segmentSize, snake.segmentSize);
+				ctx.strokeRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake.segmentSize, snake.segmentSize);
+			});
 
-		ctx.fillStyle = 'blue';
-		ctx.strokeStyle = "white";
+		})
 
-		snake2.segments.forEach(function(segment, index){
-			ctx.fillRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake2.segmentSize, snake2.segmentSize);
-			ctx.strokeRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake2.segmentSize, snake2.segmentSize);
-		});
+		// ctx.fillStyle = 'blue';
+		// ctx.strokeStyle = "white";
+
+		// snake2.segments.forEach(function(segment, index){
+		// 	ctx.fillRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake2.segmentSize, snake2.segmentSize);
+		// 	ctx.strokeRect(segment.x * board.cellWidth, segment.y * board.cellWidth, snake2.segmentSize, snake2.segmentSize);
+		// });
 
 
 		ctx.fillStyle = 'red';
 		ctx.fillRect(food.coordinates.x * board.cellWidth, food.coordinates.y * board.cellWidth, 10, 10);
-		ctx.strokeRect(food.coordinates.x * board.cellWidth, food.coordinates.y * board.cellWidth, snake1.segmentSize, snake1.segmentSize);
+		ctx.strokeRect(food.coordinates.x * board.cellWidth, food.coordinates.y * board.cellWidth, 10, 10);
 	}
 
-	this.clear = (ctx, canvas) => {
+	this.clear = (canvas, ctx) => {
 		ctx.fillStyle = 'black';
 		ctx.fillRect(0,0, canvas.height, canvas.width);
 		
